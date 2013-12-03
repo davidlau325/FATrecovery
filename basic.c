6be8cf56ef2a0a8fcf1275bb7bf88a64cab8aa76
David Lau <vx.drizzle@gmail.com>
2013-12-03 21:50:52 +0800

    Updated with milestone 1

--- basic.c
+++ basic.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "basic.h"
 
 char* devicename = NULL;
@@ -7,8 +8,72 @@ char* filename = NULL;
 char* md5 = NULL;
 
 char detectArgv(int argc,char** argv){
+    int i;
+    int hasD = 0;
+    int hasI = 0;
+    int hasL = 0;
+    int hasR = 0;
+    int hasr = 0;
+    int hasM = 0;
+
     if(argc < 4){
         return 0;
+    }else{
+    	for(i = 1; i < argc; i++){
+    		if(hasD == 2){
+    			if(argv[i][0]=='-'){
+    				return 0;
+    			}else{
+    				devicename = argv[i];
+    				hasD=1;
+    			}
+    		}else if(hasr == 2){
+    			if(argv[i][0]=='-'){
+    				return 0;
+    			}else{
+    				filename = argv[i];
+    				hasr=1;
+    			}
+    		}else if(hasR == 2){
+    			if(argv[i][0]=='-'){
+    				return 0;
+    			}else{
+    				filename = argv[i];
+    				hasR=1;
+    			}
+    		}else if(hasM == 2){
+    			if(argv[i][0]=='-'){
+    				return 0;
+    			}else{
+    				md5 = argv[i];
+    				hasM=1;
+    			}
+    		}else if(strcmp(argv[i],"-d")==0){
+    			if(hasD == 0){ hasD = 2; }else{ return 0; }
+    		}else if(strcmp(argv[i],"-r")==0){
+    			if(hasr == 0){ hasr = 2; }else{ return 0; }
+    		}else if(strcmp(argv[i],"-R")==0){
+    			if(hasR == 0){ hasR = 2; }else{ return 0; }
+    		}else if(strcmp(argv[i],"-m")==0){
+    			if(hasM == 0){ hasM = 2; }else{ return 0; }
+    		}else if(strcmp(argv[i],"-i")==0){
+    			if(hasI == 0){ hasI = 1; }else{ return 0; }
+    		}else if(strcmp(argv[i],"-l")==0){
+    			if(hasL == 0){ hasL = 1; }else{ return 0; }
+    		}else{
+    			return 0;
+    		}
+    	}
+    	if(hasD == 0 || hasD == 2){ return 0;}
+    	if((hasI + hasL + hasR + hasr)>1){ return 0; }
+    	if((hasM + hasI) > 1 || (hasM + hasL) > 1 || (hasM + hasR) > 1){ return 0;}
+    	if(hasI == 1){ return 'i';}
+    	if(hasL == 1){ return 'l';}
+		if(hasr == 1){ if(hasM == 1){ return 'm';}
+					   else if(hasM == 2){ return 0; }
+					   else{return 'r';}
+					 }
+		if(hasR == 1){ return 'R';}
+		return 0;
     }
-    return 1;
 }
--- main.c
+++ main.c
@@ -4,6 +4,14 @@
 #include "basic.h"
 #include "recovery.h"
 
+/*
+available inputs:
+
+extern char* devicename;
+extern char* filename;
+extern char* md5;
+*/
+
 int main(int argc, char** argv) {
     char detect;
     detect = detectArgv(argc,argv);
@@ -14,7 +22,39 @@ int main(int argc, char** argv) {
         printf("-r filename [-m md5]  File recovery with 8.3 filename\n");
         printf("-R filename           File recovery with long filename\n");
     }else{
-        printf("Pass!\n");
+        switch(detect){
+            case 'i':
+                printf("reached i\n");
+                printf("Device Name: %s\n",devicename);
+                // print boot sector information
+                break;
+            case 'l':
+                printf("reached l\n");
+                printf("Device Name: %s\n",devicename);
+                // list all the directory entries
+                break;
+            case 'r':
+                printf("reached r\n");
+                printf("Device Name: %s\n",devicename);
+                printf("File Name: %s\n",filename);
+                // recovery 8.3 filename
+                break;
+            case 'm':
+                printf("reached m\n");
+                printf("Device Name: %s\n",devicename);
+                printf("File Name: %s\n",filename);
+                printf("MD5: %s\n",md5);
+                // recovery 8.3 filename with MD5
+                break;
+            case 'R':
+                printf("reached R\n");
+                printf("Device Name: %s\n",devicename);
+                printf("File Name: %s\n",filename);
+                // recovery long filename
+                break;
+            default:
+                printf("reached to Default, something wrong with interpreter\n");
+        }
     }
 
     return 0;
